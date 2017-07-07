a7f0907e2bd35692122df84d40718db4448a35a2
Qian Qiao <qian.qiao@gmail.com>
2017-07-07 00:27:59 -0400

    Making sure we ignore appengine

--- webapp_noappengine.go
+++ webapp_noappengine.go
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// +build !appengine
+
 package webapp
 
 // IsDev whether the application is running in the development mode.
