14d9a6ed787eaaabcdd495bf9d8a597ac9e2be15
Qian Qiao <qian.qiao@gmail.com>
2023-05-24 21:39:16 +0000

    Fixed documentation

--- auth/doc.go
+++ auth/doc.go
@@ -12,5 +12,5 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package auth contains all functions regarding user authentication.
+// Package auth contains all functions related to user authentication.
 package auth
